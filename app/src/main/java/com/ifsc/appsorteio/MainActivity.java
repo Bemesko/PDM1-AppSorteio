293179ab641d18c75fef5d4c3d8df8f89048a726
Bernardo Mesko <bemesko@gmail.com>
2021-02-12 23:09:25 -0300

    Método de número aleatório funcionando

--- app/src/main/java/com/ifsc/appsorteio/MainActivity.java
+++ app/src/main/java/com/ifsc/appsorteio/MainActivity.java
@@ -1,8 +1,10 @@
 package com.ifsc.appsorteio;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.util.Random;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     //Poderia extender Activity mas perderia retrocompatibilidade
@@ -11,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void randomNumber(View view){
+        int initialValue=6, finalValue=10;
+
+        int randomNumber = (new Random().nextInt(finalValue - initialValue + 1))+initialValue;
+        Log.i("RandomValue", Integer.toString(randomNumber));
     }
 }
