3aaa0d38fbd2c26588467b9ad1c1e5e143bded5a
Bernardo Mesko <bemesko@gmail.com>
2021-02-12 23:18:37 -0300

    Tutorial Terminado

--- app/src/main/java/com/ifsc/appsorteio/MainActivity.java
+++ app/src/main/java/com/ifsc/appsorteio/MainActivity.java
@@ -5,20 +5,34 @@ import java.util.Random;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     //Poderia extender Activity mas perderia retrocompatibilidade
+
+    EditText txtLowValue, txtHighValue;
+    TextView textViewResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtLowValue=findViewById(R.id.editTextMinValue);
+        txtHighValue=findViewById(R.id.editTextMaxValue);
+        textViewResult = findViewById(R.id.textViewResult);
     }
 
     public void randomNumber(View view){
-        int initialValue=6, finalValue=10;
+        int lowValue=0, highValue=0;
 
-        int randomNumber = (new Random().nextInt(finalValue - initialValue + 1))+initialValue;
+        lowValue = Integer.parseInt(txtLowValue.getText().toString());
+        highValue = Integer.parseInt(txtHighValue.getText().toString());
+
+        int randomNumber = (new Random().nextInt(highValue - lowValue + 1))+lowValue;
         Log.i("RandomValue", Integer.toString(randomNumber));
+
+        textViewResult.setText(Integer.toString(randomNumber));
     }
 }
